378a7d493bf50e1efc3c64f453d93531a33db1fa
Ulik <ulan88195@gmail.com>
2024-02-17 19:41:47 +0600

    Добавил js для тестинга

---
+++ script.js
@@ -0,0 +1,5 @@
+const nameUser = prompt(`Пожалуйта введите свое имя чтобы мы могли обращаться к вам:`);
+
+var nameElement = document.getElementById("name");
+
+  nameElement.textContent = nameUser;
