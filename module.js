dccfa239099fd1ce2fddfe11b940f06b6121b535
Ulik <ulan88195@gmail.com>
2024-02-20 15:50:58 +0600

    update

--- module.js
+++ module.js
@@ -1,21 +1,9 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import { Sandpack } from "@codesandbox/sandpack-react";
+import FirstGame from "./FirstGame"
 
 const root = createRoot(document.getElementById("root"));
 
 
-export default function App() {
-  return (
-    <Sandpack 
-      template="react"
-      files={{
-        "/App.js": `export default function App() {
-  return <h1>Hello Sandpack</h1>
-}`,
-      }}
-    />
-  );
-}
-
-root.render(App());
+root.render(<FirstGame />);
